85eb5346de45eb647dc5414cd55cfd3cfc72a621
petrol95 <petrol195@yandex.ru>
2019-04-23 17:27:20 +0300

    Initial commit

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,7 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class MainClass {
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+    }
+}
